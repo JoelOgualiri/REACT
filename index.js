a41f7c50334382b65d6db157b4467043ac4ea5ce
JoelOgualiri <j.ogualiri@gmail.com>
2021-12-07 09:01:46 -0500

    put it in a function

--- index.js
+++ index.js
@@ -13,18 +13,23 @@ ReactDOM.render(page, document.getElementById('root'))
 
 
 //creating a fun facts about React
+function reactStat() {
+    return (
+        <div>
+            <img src="react-logo.png" style={{ width: 50 }}></img>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
 
-page = (<div>
-    <img src="react-logo.png" style={{ width: 50 }}></img>
-    <h1>Fun facts about React</h1>
-    <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
+        </div>
+    )
 
-</div>)
+}
 
-ReactDOM.render(page, document.getElementById('root'))
+
+ReactDOM.render(reactStat(), document.getElementById('root'))
