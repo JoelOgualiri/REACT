52fb0a19cc2fd8ebad0eff75ecb3b89d223badab
JoelOgualiri <j.ogualiri@gmail.com>
2021-12-07 00:19:21 -0500

    minimal page

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const welcome = (<h1>Hello World</h1>)
+const page = (<div>
+    <h1 className="header"> This is JSX</h1>
+    <p>This is a paragraph</p>
+</div>)
+
+ReactDOM.render(page, document.getElementById('root'))
+//ReactDOM.render(welcome, document.getElementById('root'))
+
+
+//console.log(element.props)
