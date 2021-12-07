ba53d7253f82cc564f6ced7e7defc6dcffdaafd8
JoelOgualiri <j.ogualiri@gmail.com>
2021-12-07 08:53:55 -0500

    done react tutorial page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const welcome = (<h1>Hello World</h1>)
-const page = (<div>
+var page = (<div>
     <h1 className="header"> This is JSX</h1>
     <p>This is a paragraph</p>
 </div>)
@@ -10,3 +10,21 @@ ReactDOM.render(page, document.getElementById('root'))
 
 
 //console.log(element.props)
+
+
+//creating a fun facts about React
+
+page = (<div>
+    <img src="react-logo.png" style={{ width: 50 }}></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+
+</div>)
+
+ReactDOM.render(page, document.getElementById('root'))
