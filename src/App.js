fae7c3578cf3c9d090ba463b236df8300d3dc020
JoelOgualiri <j.ogualiri@gmail.com>
2021-12-08 22:39:22 -0500

    added photo grid and text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from './Navbar';
-
+import Hero from './Hero';
 function App() {
   return (
     <div className="App">
 
       <Navbar />
+      <Hero />
+
     </div>
   );
 }
