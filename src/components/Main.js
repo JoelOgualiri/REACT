6ac954a0014c03b51a99a9de972d83b54182d41a
JoelOgualiri <j.ogualiri@gmail.com>
2021-12-08 19:26:57 -0500

    completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+
+
+import { faFacebookF, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export default function Main() {
     return (
         <main>
@@ -6,17 +12,27 @@ export default function Main() {
             <h2 className="title--job">Frontend Developer</h2>
 
             <div className="contact">
-                <button className="email">Email</button>
-                <button className="linkedIn">LinkedIn</button>
+                <button className="email"><span><FontAwesomeIcon icon={faEnvelope} /></span> Email</button>
+                <button className="linkedIn"><span><FontAwesomeIcon icon={faLinkedin} /></span>LinkedIn</button>
             </div>
 
             <div className="bio">
                 <h1>About</h1>
-                <p>I have over 2 years of experience in React, CSS, JavaScript, HTML, and JEST. I love learning new technologies.  </p>
+                <p>I have over 2 years of experience in React, CSS, JavaScript, HTML, and JEST. I am an avid student of technology.  </p>
                 <h1>Interests</h1>
                 <p>Reader. Travel geek. Chess player. Coffee fanatic. Artist.   </p>
 
             </div>
+            <div className="footer">
+                <footer>
+
+                    <FontAwesomeIcon icon={faFacebookF} />
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    <FontAwesomeIcon icon={faInstagram} />
+                    <FontAwesomeIcon icon={faEnvelope} />
+                </footer>
+            </div>
+
 
         </main>
     )
