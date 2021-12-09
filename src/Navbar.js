46e9dff61389979920f4e0d66dac3819c7f8c770
JoelOgualiri <j.ogualiri@gmail.com>
2021-12-08 21:41:46 -0500

    setup and added navbar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import logo from "./logo.jpg";
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="Navbar">
+                <img className="App-logo" src={logo} alt="logo"></img>
+            </nav>
+
+
+
+        </div>
+
+    )
+}
