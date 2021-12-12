05e1eb1ef0809aef43480a3579a1a76bda0a742a
JoelOgualiri <j.ogualiri@gmail.com>
2021-12-12 17:44:55 -0500

    edited readme

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,5 @@
 import star from "./star.png"
-//import backgroundPix from "./images/katie-zaferes.png"//
+
 export default function Card(props) {
     let badgeText
     if (props.openSpots === 0) {
@@ -7,29 +7,19 @@ export default function Card(props) {
     } else if (props.location === "Online") {
         badgeText = "ONLINE"
     }
-    console.log(props)
+
     return (
-
-
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-
             <img className="card--image" src={props.coverImg} width="50" alt="pix"></img>
             <div className="card--stats">
                 <img className="card--star" src={star} alt="star"></img>
                 <span className="gray">{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) •</span>
                 <span className="gray">{props.location}</span>
-
             </div>
             <p className="card--title">{props.comment}</p>
             <p className="card--price"><span className="bold">From ${props.price} </span> / person</p>
-
-
         </div>
-
-
-
-
     )
 }
